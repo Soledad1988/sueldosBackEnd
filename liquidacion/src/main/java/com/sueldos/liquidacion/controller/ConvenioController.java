55baaaa8e244997472903d7f76defeeec72fb3e3
Brenda <brentorres26@gmail.com>
2023-07-01 21:27:03 -0300

    Capa Controller convenio

---
+++ liquidacion/src/main/java/com/sueldos/liquidacion/controller/ConvenioController.java
@@ -0,0 +1,31 @@
+package com.sueldos.liquidacion.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sueldos.liquidacion.model.Convenio;
+import com.sueldos.liquidacion.service.ConvenioService;
+
+@RestController
+@RequestMapping("/convenio")
+public class ConvenioController {
+
+	@Autowired
+	private ConvenioService convenioService;
+	
+	@PostMapping
+	public void crear(Convenio convenio) {
+		System.out.println(convenio);
+		convenioService.crear(convenio);
+	}
+	
+	@GetMapping
+	public List<Convenio>listar(){
+		return convenioService.listar();
+	}
+}
