865a246d0724f773a1610a4b44de34a6f135b15c
Brenda <brentorres26@gmail.com>
2023-07-20 22:04:14 -0300

    Configuracion de CORS

--- liquidacion/src/main/java/com/sueldos/liquidacion/LiquidacionApplication.java
+++ liquidacion/src/main/java/com/sueldos/liquidacion/LiquidacionApplication.java
@@ -2,6 +2,9 @@ package com.sueldos.liquidacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LiquidacionApplication {
@@ -9,5 +12,21 @@ public class LiquidacionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LiquidacionApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                    @Override
+public void addCorsMappings(CorsRegistry corsRegistry) {
+corsRegistry.addMapping("/**")
+        .allowedOrigins("http://localhost:4200")
+        .allowedMethods("*")
+        .allowedHeaders("*")
+        .exposedHeaders("Authorization")
+        .allowCredentials(true).maxAge(3600);
+}
+   
+            };
+    }   
 
 }
