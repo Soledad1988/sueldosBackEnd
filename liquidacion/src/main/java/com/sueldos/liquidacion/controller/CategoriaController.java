97080e8c4a86835f5c9137095f8b6512b671b4c8
Brenda <brentorres26@gmail.com>
2023-07-22 11:56:08 -0300

    Capa controller calase categoria

---
+++ liquidacion/src/main/java/com/sueldos/liquidacion/controller/CategoriaController.java
@@ -0,0 +1,51 @@
+package com.sueldos.liquidacion.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sueldos.liquidacion.model.Categoria;
+import com.sueldos.liquidacion.service.CategoriaService;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@PostMapping
+	public void crear(@RequestBody Categoria categoria) {
+		categoriaService.crear(categoria);;
+	}
+	
+	@GetMapping
+	public List<Categoria>listar(){
+		return categoriaService.listar();
+	}
+	
+	@GetMapping("/{idCatgeoria}")
+    public Categoria get(@PathVariable("idCategoria") Integer idCategoria){
+       return categoriaService.buscar(idCategoria);
+    }
+   
+	@DeleteMapping("/{idCategoria}")
+	void borrar(@PathVariable Integer idCategoria){
+		categoriaService.borrar(idCategoria);
+	}
+  
+	@PutMapping
+	public void actualizar(@RequestBody Categoria categoria){
+		categoriaService.actualizar(categoria);
+	 }
+
+
+}
